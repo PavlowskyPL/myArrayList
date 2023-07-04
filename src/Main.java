04d91a36f09afc719e32e1535bae0bdd536660e4
Kochubei Pavel <pavel.kochybeu@gmail.com>
2023-07-04 15:28:10 +0300

    add comments

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,29 @@
 public class Main {
-    public static void main(String[]args)
-    {
+    public static void main(String[] args) {
         MyArrayList<String> words = new MyArrayList<String>();
-        System.out.println(words);		//should show []
+        System.out.println(words);        // []
         words.add("A");
         words.add("B");
         words.add("C");
-        System.out.println(words);		//should show [A, B, C]
-        for(int i=0; i<10; i++)
-            words.add(""+i);
-        System.out.println(words);		//should show [A, B, C, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        System.out.println(words.size());	//should show 13
-        System.out.println(words.get(2));	//should show C
+        System.out.println(words);        // [A, B, C]
+        for (int i = 0; i < 10; i++)
+            words.add("" + i);
+        System.out.println(words);        // [A, B, C, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        System.out.println(words.size());    // 13
+        System.out.println(words.get(2));    // C
         words.set(2, "Z");
         String temp = words.remove(0);
-        System.out.println(temp);				//should show A
+        System.out.println(temp);                // A
         words.remove(3);
-        System.out.println(words);		//should show [B, Z, 0, 2, 3, 4, 5, 6, 7, 8, 9]
+        System.out.println(words);        // [B, Z, 0, 2, 3, 4, 5, 6, 7, 8, 9]
         words.add(2, "X");
-        System.out.println(words);		// [B, Z, X, 0, 2, 3, 4, 5, 6, 7, 8, 9]
+        System.out.println(words);        // [B, Z, X, 0, 2, 3, 4, 5, 6, 7, 8, 9]
         // Testing the capacity doubling and halving
         MyArrayList<Integer> nums = new MyArrayList<Integer>();
-        for(int i=0; i<10; i++)
+        for (int i = 0; i < 10; i++)
             nums.add(i);
-        System.out.println(nums.bufferSize());	//should show 10
+        System.out.println(nums.bufferSize());    // 10
         nums.add(10);
-        System.out.println(nums.bufferSize());    //should show 20
+        System.out.println(nums.bufferSize());    // 20
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,40 +1,33 @@
-public class MyArrayList<anyType> implements ListInterface<anyType>
-{
-    private Object[] list;		//stores the actual elements
-    private int numElements;	//used to keep track of the number of valid elements in the list
+public class MyArrayList<anyType> implements ListInterface<anyType> {
+    private Object[] list;        //stores the actual elements
+    private int numElements;    //used to keep track of the number of valid elements in the list
 
-    public MyArrayList()
-    {
-        list = new Object[10];	//start with a buffer size of 10
+    public MyArrayList() {
+        list = new Object[10];    //start with a buffer size of 10
         numElements = 0;
     }
 
-    private void doubleCapacity()	//private because this is a helper method that need not be used outside of the class
+    private void doubleCapacity()    //private because this is a helper method that need not be used outside of the class
     {
-        //make list twice as big, i.e. given [A, B, C, null], results with [A, B, C, null, null, null, null, null]
-        //to be used if we add an element that would be over the capacity of list
-        Object[] temp = new Object[list.length*2];
+
+        Object[] temp = new Object[list.length * 2];
         System.arraycopy(list, 0, temp, 0, list.length);
         list = temp;
 
     }
 
-    private void cutCapacity()	//private because this is a helper method that need not be used outside of the class
+    private void cutCapacity()    //private because this is a helper method that need not be used outside of the class
     {
-        //make list half as big, i.e. given [A, B, C, null, null, null, null, null], results with [A, B, C, null]
-        //to be used if after removing an element, we have less than 1/3 of the capacity of list being used
-        Object[] temp = new Object[list.length/2];
-        for(int i=0; i<temp.length; i++)
+        Object[] temp = new Object[list.length / 2];
+        for (int i = 0; i < temp.length; i++)
             temp[i] = list[i];
         list = temp;
     }
 
-    public String toString()
-    {
+    public String toString() {
         String ans = "[";
-        //add all array elements with a comma separating each, i.e. [A, B, C]
-        for (int i = 0; i < numElements; i++)
-        {
+        // [A, B, C]
+        for (int i = 0; i < numElements; i++) {
             ans += list[i];
             if (i < numElements - 1)
                 ans += ", ";
@@ -65,8 +58,8 @@ public class MyArrayList<anyType> implements ListInterface<anyType>
     public int size() {
         return numElements;
     }
-    public int bufferSize()
-    {
+
+    public int bufferSize() {
         return list.length;
     }
 
